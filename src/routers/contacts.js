f3a07e5d8b133349808b0cf4e9722ef09f15ac76
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-08-19 04:58:53 -0600

    add ctrlWrapper and conneect http-error library

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,11 +3,12 @@ import {
   getContactsController,
   getContactByIdController,
 } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', getContactsController);
+contactsRouter.get('/', ctrlWrapper(getContactsController));
 
-contactsRouter.get('/:contactId', getContactByIdController);
+contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 export default contactsRouter;
