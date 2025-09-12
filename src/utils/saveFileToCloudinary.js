a67bacf44cbe5e86810339d0dcf8ca0f1aa7f63f
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-09-12 06:21:15 -0600

    fix

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -13,7 +13,7 @@ cloudinary.v2.config({
 
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path, {
-    folder: 'temp_images',
+    folder: 'images',
     use_filename: true,
   });
   await fs.unlink(file.path);
